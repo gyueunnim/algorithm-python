95b8c179ce318c1f91a2c8dc3d2ebf94f2ee7102
gyueunnim <rnrmfrbdms@gmail.com>
2023-06-23 17:17:57 +0900

    assignment1

---
+++ python/computing_thinking_worksheet/worksheet1.py
@@ -0,0 +1,17 @@
+# worksheet 1
+
+# 1
+apple = 1500
+pear = 2500
+tangerine = 500
+
+print((apple * 100) + (pear * 12) + (tangerine * 500), "원");
+
+
+#2
+print(35*3.30579,"제곱미터")
+
+
+#3
+exchangeRate = 163.42
+print("한국 돈 150만원은 ", 1500000 * exchangeRate, "위안이다.")
