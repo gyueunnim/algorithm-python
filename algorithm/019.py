67d5fde67d287995e4b7c79c4f12d65a737d0378
gyueunnim <rnrmfrbdms@gmail.com>
2023-10-29 19:11:30 +0900

    Do it 019, 020 임시 풀이

---
+++ algorithm/019.py
@@ -0,0 +1,9 @@
+# K번째 수 구하기 (백준 11004)
+
+'''
+n, i = map(int, input().split())
+a = list(map(int, input().split()))
+
+a.sort()
+print(a[i - 1])
+'''
---
+++ algorithm/020.py
@@ -0,0 +1,11 @@
+# 수 정렬하기2 (백준 2751)
+'''
+n = int(input())
+a = [0] * n
+for i in range(n):
+    a[i] = int(input())
+
+a.sort()
+for n in a:
+    print(n)
+'''
